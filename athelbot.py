0fcee1ab1057b9198d2876dfac1e740c1d9686db
AthelGS <33658571+AthelGS@users.noreply.github.com>
2019-01-05 16:56:53 -0500

    Update athelbot.py

--- athelbot.py
+++ athelbot.py
@@ -4,6 +4,17 @@ import random
 from discord import Game
 from discord.ext.commands import Bot
 
+quotes = []
+if os.path.exists("athelbotcfg.txt"):
+	with open("athelbotcfg.txt") as f:
+		for line in f:
+			line = line.strip()
+			quotes.append(line)
+else:
+	quotes = ["Test quote 1",
+	"Test quote 2",
+	"Test quote 3"]
+	
 BOT_PREFIX = ('a-')
 TOKEN = os.environ['BOT_TOKEN']
 
@@ -12,5 +23,19 @@ client = Bot(command_prefix = BOT_PREFIX)
 @client.command()
 async def test():
 	await client.say("pong")
-	
+
+async def quote():
+	await client.say("quote")
+
 client.run(TOKEN)
+
+
+if os.path.exists("athelbotcfg.txt"):
+	os.remove("athelbotcfg.txt")
+
+f = open("athelbotcfg.txt", "w+")
+
+for item in quotes:
+	f.write("%s\n" % item);
+	
+f.close()
