de891b11389f155b676af338369c0bf3bd8f325e
AthelGS <33658571+AthelGS@users.noreply.github.com>
2019-01-05 16:50:42 -0500

    beta athelbot refined

--- athelbot.py
+++ athelbot.py
@@ -1,114 +1,16 @@
-# from boto.s3.connection import S3Connection
-
 import os
-
 import discord
 import random
+from discord import Game
+from discord.ext.commands import Bot
 
-quotes = []
-if os.path.exists("athelbotcfg.txt"):
-	with open("athelbotcfg.txt") as f:
-		for line in f:
-			line = line.strip()
-			quotes.append(line)
-else:
-	quotes = ["Test quote 1",
-	"Test quote 2",
-	"Test quote 3"]
+BOT_PREFIX = ('a-')
+TOKEN = os.environ['BOT_TOKEN']
 
-quotelen = len(quotes) - 1
-print (quotelen)
+client = Bot(command_prefix = BOT_PREFIX)
 
-#SPACE_MAGIC = S3Connection(os.environ['SECRET_CODE_THING'])
-
-bot_token = os.environ['BOT_TOKEN']
-
-client = discord.Client()
-activity = discord.Game(name="with the API")
-
-@client.event
-async def on_message(message):
-	quotelen = len(quotes) - 1
-	if message.author == client.user:
-		return
+@client.command()
+async def test():
+	await client.say("pong")
 	
-	if message.content == ('a-testthis'):
-		msg = "Whaddya know, it works!"
-		await client.send_message(message.channel, msg)
-		
-	if message.content.startswith('a-quote'): # Send a random quote
-		quchoice = random.randint(0, quotelen)
-		embed = discord.Embed(title="Athel Quote", description=quotes[quchoice], color=0x00ffff)
-		embed.set_footer(text= "Quote #" + str(quchoice) + " of " + str(quotelen) + " quotes.")
-		# msg = str(random.choice(quotes)).format(message)
-		await client.send_message(message.channel, embed=embed)
-	
-	if message.content.startswith('a-embedtest'): # Testing how to do embeds
-		embed = discord.Embed(title="Title", description="Desc", color=0x00ff00)
-		embed.add_field(name="Field1", value="test", inline=False)
-		embed.add_field(name="Field2", value=str(random.choice(quotes)), inline=False)
-		embed.set_footer(text="testfooter")
-		await client.send_message(message.channel, embed=embed)
-		
-	if message.content.startswith('a-help'): # Help command
-		embed = discord.Embed(title="AthelBot commands", description="All command prefixes are 'a-', keep this in mind.", color=0x00ffff)
-		embed.add_field(name="help", value="Displays this help dialogue.", inline=False)
-		embed.add_field(name="quote", value="Picks a random Athel quote.", inline=False)
-		embed.add_field(name="delquote", value="Deletes a quote by number.", inline=False)
-		embed.add_field(name="addquote", value="Add a quote to Athel quotes.", inline=False)
-		embed.add_field(name="qnum", value="Pick a quote by number.", inline=False)
-		await client.send_message(message.channel, embed=embed)
-		
-	if message.content.startswith('a-addquote'): # Add a quote
-		msg = 'Please type the quote you want to add.'.format(message)
-		print ("asked message")
-		await client.send_message(message.channel, msg)
-		msg = await client.wait_for_message(author=message.author)
-		quotes.append(str(msg.content))
-		print (quotes)
-		msg = 'Quote added! ' + str(msg.content)
-		quotelen = len(quotes)
-		await client.send_message(message.channel, msg)
-		
-	if message.content.startswith('a-qnum'): # Add a quote
-		quotelen = len(quotes) - 1
-		msg = 'This is a WIP.'
-		msg = 'Please select a quote (0 - ' + str(len(quotes)-1) + ')'
-		await client.send_message(message.channel, msg)
-		msg = await client.wait_for_message(author=message.author)
-		msg = str(msg.content)
-		embed = discord.Embed(title="Athel Quote", description=quotes[int(msg)], color=0x00ffff)
-		embed.set_footer(text= "Quote #" + str(msg) + " of " + str(len(quotes)-1) + " quotes.")
-		# msg = str(random.choice(quotes)).format(message)
-		await client.send_message(message.channel, embed=embed)
-	
-	if message.content.startswith('a-delquote'): # Deletes a quote
-		msg = 'Please select a quote (0 - ' + str(len(quotes)-1) + ')'
-		await client.send_message(message.channel, msg)
-		msg = await client.wait_for_message(author=message.author)
-		msg = str(msg.content)
-		quotes.pop(int(msg))
-		msg = 'Quote number ' + str(msg) + ' deleted!'
-		await client.send_message(message.channel, msg)
-		
-
-		
-@client.event
-async def on_ready():
-
-	print('Logged in as')
-	print(client.user.name)
-	print(client.user.id)
-	
-client.run(bot_token)
-
-if os.path.exists("athelbotcfg.txt"):
-	os.remove("athelbotcfg.txt")
-
-f = open("athelbotcfg.txt", "w+")
-
-for item in quotes:
-	f.write("%s\n" % item);
-	
-f.close()
-print("configs saved");
+client.run(TOKEN)
